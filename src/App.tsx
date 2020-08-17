01e26e8e92dce9bcf0b07ecac3616de4f9e06816
Ellis Hoag <ellis.sparky.hoag@gmail.com>
2020-08-17 00:54:56 -0700

    add Alert on errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import YAML from 'yaml';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
-import { Card, Spinner, Nav, ListGroup } from 'react-bootstrap';
+import { Alert, Card, Spinner, Nav, ListGroup } from 'react-bootstrap';
+
+const isValidIngredient = (ingredient: any) => (
+  ('name' in ingredient)
+  && ('amount' in ingredient)
+  && ('unit' in ingredient)
+);
+
+const isValidRecipe = (recipe: any) => (
+  ('title' in recipe)
+  && ('description' in recipe)
+  && ('ingredients' in recipe)
+  && recipe.ingredients.every(isValidIngredient)
+  && ('directions' in recipe)
+);
 
 const Ingredient: React.FC<{ingredient: any}> = ({ingredient}) => {
   // TODO: Convert units.
@@ -14,15 +28,26 @@ const Ingredient: React.FC<{ingredient: any}> = ({ingredient}) => {
 
 const Recipe: React.FC<{recipeName: string}> = ({recipeName}) => {
   const [recipe, setRecipe] = useState<any>();
+  const [errorMessage, setErrorMessage] = useState<string>();
 
   useEffect(() => {
     fetch(process.env.PUBLIC_URL + 'recipes/' + recipeName + '.yaml')
       .then(response => response.text())
-      .then(data => setRecipe(YAML.parse(data)));
+      .then(data => {
+        const recipe = YAML.parse(data);
+        if (isValidRecipe(recipe)) {
+          setRecipe(recipe)
+        } else {
+          throw new Error("Unable to parse YAML.");
+        }
+      })
+      .catch(error => setErrorMessage(error.message));
   }, [recipeName]);
 
   return (
-    recipe ? (
+    errorMessage ? (
+      <Alert variant='danger'>Could not find recipe for "{recipeName}"</Alert>
+    ) : recipe ? (
       <div>
         <Card.Title as="h1">{recipe.title}</Card.Title>
         <Card.Text>{recipe.description}</Card.Text>
@@ -85,6 +110,12 @@ const About: React.FC = () => {
   );
 };
 
+const BadURL: React.FC = () => {
+  return (
+    <Alert variant='danger'>Invalid URL.</Alert>
+  );
+};
+
 const Loading: React.FC = () => {
   // TODO: Center spinner.
   return (
@@ -92,7 +123,7 @@ const Loading: React.FC = () => {
       <span className="sr-only">Loading...</span>
     </Spinner>
   );
-}
+};
 
 const App: React.FC = () => {
   return (
@@ -115,6 +146,7 @@ const App: React.FC = () => {
             <Route path='/:recipeName' exact component={(props: any) =>
               <Recipe recipeName={props.match.params.recipeName} />
             } />
+            <Route component={BadURL} />
           </Switch>
         </Card.Body>
       </Card>
