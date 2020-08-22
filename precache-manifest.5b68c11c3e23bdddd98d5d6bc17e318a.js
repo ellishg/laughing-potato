4107a5fb1fe4800ad9b48b78c97a82b42183ccfa
Ellis Hoag <ellis.sparky.hoag@gmail.com>
2020-08-22 12:34:54 -0700

    Updates

---
+++ precache-manifest.5b68c11c3e23bdddd98d5d6bc17e318a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc56230e181ba35970d258d5887486ee",
+    "url": "/laughing-potato/index.html"
+  },
+  {
+    "revision": "9c78cf1b2aeff4fd2c80",
+    "url": "/laughing-potato/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9c78cf1b2aeff4fd2c80",
+    "url": "/laughing-potato/static/js/2.d5829824.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/laughing-potato/static/js/2.d5829824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd8be9cac3c7481d62f",
+    "url": "/laughing-potato/static/js/main.36b442c2.chunk.js"
+  },
+  {
+    "revision": "32589a240ecdcba504f7",
+    "url": "/laughing-potato/static/js/runtime-main.a9d8b44f.js"
+  }
+]);
