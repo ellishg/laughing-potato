2a86418b7b37da160b9f7b631c7fb81232ae84ab
Ellis Hoag <ellis.sparky.hoag@gmail.com>
2020-08-22 13:42:07 -0700

    testing

---
+++ precache-manifest.57bf4ad8bc268fcb0ae649a6544b43b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ec1c0c6b850eb012c5e12e66b695be",
+    "url": "/ellishg.github.io/laughing-potato/index.html"
+  },
+  {
+    "revision": "395d0c89823ffd4295cb",
+    "url": "/ellishg.github.io/laughing-potato/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "395d0c89823ffd4295cb",
+    "url": "/ellishg.github.io/laughing-potato/static/js/2.e0a4bbea.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/ellishg.github.io/laughing-potato/static/js/2.e0a4bbea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c50eca484f1648583bb6",
+    "url": "/ellishg.github.io/laughing-potato/static/js/main.ea0336a9.chunk.js"
+  },
+  {
+    "revision": "6e20ee6556934848fa39",
+    "url": "/ellishg.github.io/laughing-potato/static/js/runtime-main.3532b58d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1a11d559160bb6b72eead181579f10fc.js"
+  "/ellishg.github.io/laughing-potato/precache-manifest.57bf4ad8bc268fcb0ae649a6544b43b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ellishg.github.io/laughing-potato/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
